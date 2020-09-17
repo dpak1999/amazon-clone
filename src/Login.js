3e06f9a276ca659c57aa737182205303e1168c26
dpak1999 <deepakkumardash313@gmail.com>
2020-09-17 19:29:34 +0530

    handling login

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { auth } from "./firebase";
 import "./Login.css";
 
 function Login() {
+  const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const signIn = (e) => {
     e.preventDefault();
     // dope firebase login shit goes here
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
   };
 
   const register = (e) => {
@@ -20,6 +27,9 @@ function Login() {
       .createUserWithEmailAndPassword(email, password)
       .then((auth) => {
         console.log(auth);
+        if (auth) {
+          history.push("/");
+        }
       })
       .catch((error) => alert(error.message));
   };
